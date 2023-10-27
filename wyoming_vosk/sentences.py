1f025b1e486789ee8626acebc05dd04c17cb673d
Michael Hansen <mike@rhasspy.org>
2023-10-27 12:13:24 -0500

    Fix list item output

--- wyoming_vosk/sentences.py
+++ wyoming_vosk/sentences.py
@@ -1,8 +1,14 @@
+import itertools
 import logging
 import re
 from dataclasses import dataclass, field
+from functools import partial
 from pathlib import Path
-from typing import Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Dict, Iterable, List, Optional, Tuple, Union
+
+if TYPE_CHECKING:
+    from hassil.expression import Expression, Sentence
+    from hassil.intents import SlotList
 
 _LOGGER = logging.getLogger()
 
@@ -87,8 +93,10 @@ def load_sentences_for_language(
 
             slot_list_values: List[TextSlotValue] = []
             for slot_value in slot_values:
+                values_in: List[str] = []
+
                 if isinstance(slot_value, str):
-                    value_in: str = slot_value
+                    values_in.append(slot_value)
                     value_out: str = slot_value
                 else:
                     # - in: text to say
@@ -96,9 +104,21 @@ def load_sentences_for_language(
                     value_in = slot_value["in"]
                     value_out = slot_value["out"]
 
-                slot_list_values.append(
-                    TextSlotValue(TextChunk(value_in), value_out=value_out)
-                )
+                    if hassil.intents.is_template(value_in):
+                        input_expression = hassil.parse_expression.parse_sentence(
+                            value_in
+                        )
+                        for input_text in hassil.sample.sample_expression(
+                            input_expression,
+                        ):
+                            values_in.append(input_text)
+                    else:
+                        values_in.append(value_in)
+
+                for value_in in values_in:
+                    slot_list_values.append(
+                        TextSlotValue(TextChunk(value_in), value_out=value_out)
+                    )
 
             slot_lists[slot_name] = TextSlotList(slot_list_values)
 
@@ -129,7 +149,7 @@ def load_sentences_for_language(
                     input_expression = hassil.parse_expression.parse_sentence(
                         input_template
                     )
-                    for input_text in hassil.sample.sample_expression(
+                    for input_text, output_text in sample_expression_with_output(
                         input_expression,
                         slot_lists=slot_lists,
                         expansion_rules=expansion_rules,
@@ -150,6 +170,111 @@ def load_sentences_for_language(
     _CONFIG_CACHE[language] = config
 
     return config
+
+
+def sample_expression_with_output(
+    expression: "Expression",
+    slot_lists: "Optional[Dict[str, SlotList]]" = None,
+    expansion_rules: "Optional[Dict[str, Sentence]]" = None,
+) -> Iterable[Tuple[str, Optional[str]]]:
+    """Sample possible text strings from an expression."""
+    from hassil.expression import (
+        ListReference,
+        RuleReference,
+        Sequence,
+        SequenceType,
+        TextChunk,
+    )
+    from hassil.intents import TextSlotList
+    from hassil.recognize import MissingListError, MissingRuleError
+    from hassil.util import normalize_whitespace
+
+    if isinstance(expression, TextChunk):
+        chunk: TextChunk = expression
+        yield (chunk.original_text, chunk.original_text)
+    elif isinstance(expression, Sequence):
+        seq: Sequence = expression
+        if seq.type == SequenceType.ALTERNATIVE:
+            for item in seq.items:
+                yield from sample_expression_with_output(
+                    item,
+                    slot_lists,
+                    expansion_rules,
+                )
+        elif seq.type == SequenceType.GROUP:
+            seq_sentences = map(
+                partial(
+                    sample_expression_with_output,
+                    slot_lists=slot_lists,
+                    expansion_rules=expansion_rules,
+                ),
+                seq.items,
+            )
+            sentence_texts = itertools.product(*seq_sentences)
+            for sentence_words in sentence_texts:
+                yield (
+                    normalize_whitespace("".join(w[0] for w in sentence_words)),
+                    normalize_whitespace(
+                        "".join(w[1] for w in sentence_words if w[1] is not None)
+                    ),
+                )
+        else:
+            raise ValueError(f"Unexpected sequence type: {seq}")
+    elif isinstance(expression, ListReference):
+        # {list}
+        list_ref: ListReference = expression
+        if (not slot_lists) or (list_ref.list_name not in slot_lists):
+            raise MissingListError(f"Missing slot list {{{list_ref.list_name}}}")
+
+        slot_list = slot_lists[list_ref.list_name]
+        if isinstance(slot_list, TextSlotList):
+            text_list: TextSlotList = slot_list
+
+            if not text_list.values:
+                # Not necessarily an error, but may be a surprise
+                _LOGGER.warning("No values for list: %s", list_ref.list_name)
+
+            for text_value in text_list.values:
+                if text_value.value_out:
+                    is_first_text = True
+                    for input_text, output_text in sample_expression_with_output(
+                        text_value.text_in,
+                        slot_lists,
+                        expansion_rules,
+                    ):
+                        if is_first_text:
+                            output_text = (
+                                text_value.value_out
+                                if isinstance(text_value.value_out, str)
+                                else ""
+                            )
+                            is_first_text = False
+                        else:
+                            output_text = None
+
+                        yield (input_text, output_text)
+                else:
+                    yield from sample_expression_with_output(
+                        text_value.text_in,
+                        slot_lists,
+                        expansion_rules,
+                    )
+        else:
+            raise ValueError(f"Unexpected slot list type: {slot_list}")
+    elif isinstance(expression, RuleReference):
+        # <rule>
+        rule_ref: RuleReference = expression
+        if (not expansion_rules) or (rule_ref.rule_name not in expansion_rules):
+            raise MissingRuleError(f"Missing expansion rule <{rule_ref.rule_name}>")
+
+        rule_body = expansion_rules[rule_ref.rule_name]
+        yield from sample_expression_with_output(
+            rule_body,
+            slot_lists,
+            expansion_rules,
+        )
+    else:
+        raise ValueError(f"Unexpected expression: {expression}")
 
 
 def correct_sentence(
@@ -177,12 +302,15 @@ def correct_sentence(
         scorer_kwargs={"weights": (1, 1, 3)},
     )
     fixed_text, score = result[0], result[1]
-    _LOGGER.debug(
-        "score=%s/%s, original=%s, fixed=%s", score, score_cutoff, text, fixed_text
-    )
 
     if (score_cutoff <= 0) or (score <= score_cutoff):
         # Map to output text
-        return config.sentences[fixed_text]
+        final_text = config.sentences[fixed_text]
+    else:
+        final_text = text
 
-    return text
+    _LOGGER.debug(
+        "score=%s/%s, original=%s, final=%s", score, score_cutoff, text, final_text
+    )
+
+    return final_text
